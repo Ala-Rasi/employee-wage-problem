7dba6e64450b4c0b592b5f1c15598b2fde82bc6f
Ajitesh <ajiteshmadhu09@gmail.com>
2025-02-17 12:02:03 +0530

    feat:Add Calculate Daily Employee Wage

--- employee-wage-problem.cpp
+++ employee-wage-problem.cpp
@@ -3,16 +3,34 @@
 #include <time.h>
 using namespace std;
 
+const int WAGE_PER_HOUR = 20;
+const int FULL_DAY_HOUR = 8;
+
 // UC1: Check Employee Attendance for a single day
 int checkAttendance() {
     return rand() % 2; // 0 for absent, 1 for present
 }
 
+// UC2: Calculate Daily Employee Wage
+int calculateDailyWage(int empStatus) {
+    if (empStatus == 0) {
+        cout << "Employee is Absent. No wage calculated." << endl;
+        return 0;
+    }
+    cout<<"Full-time Wages: "<<WAGE_PER_HOUR * FULL_DAY_HOUR<<endl;
+    return WAGE_PER_HOUR * FULL_DAY_HOUR;
+}
+
 int main() {
     cout << "Welcome to Employee Wage Computation Program on Master Branch" << endl;
     srand(time(0));
+
     //UC1
     int empStatus = checkAttendance();
     cout << (empStatus == 1 ? "Employee is Present" : "Employee is Absent") << endl;
+
+    //UC2
+    int fullTimeWage=calculateDailyWage(empStatus);
+    
     return 0;
 }
