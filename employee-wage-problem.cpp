ad3b47fe526eb00a4a96a17beec87b5aa5950541
Ajitesh <ajiteshmadhu09@gmail.com>
2025-02-17 11:52:34 +0530

    feat:Add Check Employee Attendance

--- employee-wage-problem.cpp
+++ employee-wage-problem.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+
+// UC1: Check Employee Attendance for a single day
+int checkAttendance() {
+    return rand() % 2; // 0 for absent, 1 for present
+}
 
 int main() {
     cout << "Welcome to Employee Wage Computation Program on Master Branch" << endl;
+    srand(time(0));
+    //UC1
+    int empStatus = checkAttendance();
+    cout << (empStatus == 1 ? "Employee is Present" : "Employee is Absent") << endl;
     return 0;
 }
